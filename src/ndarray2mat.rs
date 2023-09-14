96d9a365d7fa6d6200897ae6b39927fadadc16f3
sjbeskur <sam.beskur@gmail.com>
2023-09-13 21:36:13 -0600

    unit test mat2ndarray

--- src/ndarray2mat.rs
+++ src/ndarray2mat.rs
@@ -23,3 +23,20 @@ impl ToNDArray for cv::core::Mat {
 pub trait ToNAlgebra{
     //fn try_as_array(&self) -> Result<ArrayView2<u8>, Box<dyn Error>>;
 }
+
+
+#[test]
+fn test_to_ndarray() -> Result<(), Box<dyn std::error::Error>>{
+
+    use opencv::imgcodecs::{imread, IMREAD_GRAYSCALE};
+
+    let file = "img/sjb-aerial.png";
+    let img = imread(file, IMREAD_GRAYSCALE)?;
+    let img_nd = img.try_as_array()?;
+    println!("dimensions: {:?}", img_nd.shape());
+    println!("dimensions: {:?}", img_nd);
+    assert_eq!(img_nd.shape(), [749,502]);
+    assert_eq!(img.rows(), 749);
+    assert_eq!(img.cols(), 502);
+    Ok(())
+}
